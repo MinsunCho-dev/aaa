a6ccfbeb975c56d131fe2fdb8f924e3361d9d23e
jhj951105@gmail.com <jhj951105@gmail.com>
2023-12-13 15:30:29 +0900

    hello2

--- myapp2/src/main/java/com/example/demo/controller/HelloController.java
+++ myapp2/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-	
+	@GetMapping("/Hello2") 
+	public String hello1() {
+		System.out.println("hello");
+		return "hello";
+	}	
 }
